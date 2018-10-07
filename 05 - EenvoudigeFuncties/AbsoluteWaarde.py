044b41e6feb9706c0f02028a42f509f20b21623a
margomalfait <43027760+margomalfait@users.noreply.github.com>
2018-10-07 19:16:46 +0200

    Absolute Waarde

--- 05 - EenvoudigeFuncties/AbsoluteWaarde.py
+++ 05 - EenvoudigeFuncties/AbsoluteWaarde.py
@@ -2,6 +2,6 @@ x = float(input('x: '))
 y = float(input('y: '))
 
 linker_lid = abs(abs(x) - abs(y))
-rechter_lid = abs(x) - abs(y)
+rechter_lid = abs(x - y)
 
 print('{:.4f} ≤ {:.4f}'.format(linker_lid, rechter_lid))
