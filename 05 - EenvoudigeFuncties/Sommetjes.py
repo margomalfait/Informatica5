53352ad69a4fba99e316c41a1316e671295ade7a
margomalfait <43027760+margomalfait@users.noreply.github.com>
2018-10-09 19:33:44 +0200

    Sommetjes

--- 05 - EenvoudigeFuncties/Sommetjes.py
+++ 05 - EenvoudigeFuncties/Sommetjes.py
@@ -3,15 +3,29 @@ a = float(input('Geef een getal a tussen 1 en 20: '))
 b = float(input('Geef een getal b tussen 1 en 20: '))
 
 # berekening
-uitkomst1 = '{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(a, b, (a + b))
-uitkomst2 = '{:>6.0f} + {:<6.0f} = {:<6.0f}'.format((a * 10), (b * 10), ((a* 10) + (b * 10)))
-uitkomst3 = '{:>6.0f} + {:<6.0f} = {:<6.0f}'.format((a * 10 ** 2), (b * 10 ** 2), (a * 10 ** 2) + (b * 10 **2))
-uitkomst4 = '{:>6.0f} + {:<6.0f} = {:<6.0f}'.format((a * 10 ** 3), (b * 10 ** 3)), (a * 10 ** 3) + (b * 10 ** 3)
-uitkomst5 = '{:>6.0f} + {:<6.0f} = {:<6.0f}'.format((a * 10 ** 4), (b * 10 ** 4)), (a * 10 ** 4) + (b * 10 ** 4)
+getal_1 = (10 ** 0) * a
+getal_2 = (10 ** 0) * b
+getal_3 = getal_1 + getal_2
+
+getal_4 = (10 ** 1) * a
+getal_5 = (10 ** 1) * b
+getal_6 = getal_4 + getal_5
+
+getal_7 = (10 ** 2) * a
+getal_8 = (10 ** 2) * b
+getal_9 = getal_7 + getal_8
+
+getal_10 = (10 ** 3) * a
+getal_11 = (10 ** 3) * b
+getal_12 = getal_10 + getal_11
+
+getal_13 = (10 ** 4) * a
+getal_14 = (10 ** 4) * b
+getal_15 = getal_13 + getal_14
 
 # uitvoer
-print(uitkomst1)
-print(uitkomst2)
-print(uitkomst3)
-print(uitkomst4)
-print(uitkomst5)
+print('{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(getal_1, getal_2, getal_3))
+print('{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(getal_4, getal_5, getal_6))
+print('{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(getal_7, getal_8, getal_9))
+print('{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(getal_10, getal_11, getal_12))
+print('{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(getal_13, getal_14, getal_15))
