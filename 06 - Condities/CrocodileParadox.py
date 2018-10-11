d1a9fb181c6fdaed24e34dbf7865ac9089cea1ac
margomalfait <43027760+margomalfait@users.noreply.github.com>
2018-10-11 10:03:07 +0200

    Trolley

---
+++ 06 - Condities/CrocodileParadox.py
@@ -0,0 +1,10 @@
+zenuwen = input('Wie heeft stalen zenuwen? ')
+
+if zenuwen == 'vader':
+    print('Krokodil geeft kind terug')
+
+elif zenuwen == 'krokodil':
+    print('Krokodil eet kind op')
+
+else:
+    print('Moe van het denken')
