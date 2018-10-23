e0f39e7e7fd4e407e28f9cce170e30a351574658
margomalfait <43027760+margomalfait@users.noreply.github.com>
2018-10-23 18:27:13 +0200

    Blad steen schaar

--- 06 - Condities/Blad Steen Schaar.py
+++ 06 - Condities/Blad Steen Schaar.py
@@ -1,3 +1,13 @@
 speler_1 = input('Vorm achter rug speler 1: ')
 speler_2 = input('Vorm achter rug speler 2: ')
 
+if speler_1 == 'blad' and speler_2 == 'steen':
+    print('speler 1 wint')
+elif speler_1 == 'schaar' and speler_2 == 'blad':
+    print('speler 1 wint')
+elif speler_1 == 'steen' and speler_2 == 'schaar':
+    print('speler 1 wint')
+elif speler_1 == speler_2:
+    print('onbeslist')
+else:
+    print('speler 2 wint')
