e06ebb23e4502db0893540c08c6eef32871ab7e6
margomalfait <43027760+margomalfait@users.noreply.github.com>
2018-11-13 20:05:07 +0100

    Fibonacci

---
+++ 07a-Iteraties-For-lus/Rij van Fibonacci.py
@@ -0,0 +1,12 @@
+getal = int(input('Geef een getal: '))
+getal_1, getal_2 = 1, 1
+
+if getal > 2:
+    for i in range(3, getal + 1):
+        getal_3 = getal_1 + getal_2
+        getal_1 = getal_2
+        getal_2 = getal_3
+    print(getal_3)
+
+elif getal == 1 or getal == 2:
+    print('1')
