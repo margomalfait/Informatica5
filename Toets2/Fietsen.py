1c1094db912a314fbe25db11048b8f6d0c79bfc0
margomalfait <43027760+margomalfait@users.noreply.github.com>
2018-11-29 09:20:21 +0100

    unieke basen

--- Toets2/Fietsen.py
+++ Toets2/Fietsen.py
@@ -1,11 +1,14 @@
+# invoer
 stijn = int(input('snelheid Stijn: '))
 kaat = int(input('snelheid Kaat: '))
 x = int(input('afstand tussen beide huizen: '))
 som = 0
 tijd = 0
 
+# berekening
 while  x > som:
     som += stijn + kaat
     tijd += 1
 
+# uivoer
 print('Na ' + str(tijd) + ' s hebben Stijn en Kaat elkaar ontmoet.')
--- Toets2/Unieke basen.py
+++ Toets2/Unieke basen.py
@@ -1,2 +1,12 @@
 # invoer
 hoeveelheid = int(input('hoeveel basen: '))
+soorten = 'ATGC'
+
+# berekening
+for i in range(hoeveelheid - 1):
+    base = str(input('Base: '))
+    if base in soorten:
+         soort = base
+
+# uitvoer
+print(soort)
